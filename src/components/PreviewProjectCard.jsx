442e208f9c85c4163230e011ddbe4e71e0d5a6fb
Alexis <yernauxalex@gmail.com>
2022-10-11 16:28:19 +0200

    Fix missing project type

--- src/components/PreviewProjectCard.jsx
+++ src/components/PreviewProjectCard.jsx
@@ -12,12 +12,19 @@ function PreviewProjectCard(props) {
     return (
         <>{data.map(({ id, name, desc, pathBanner, urlGithub, urlHost, frontend, backend, type }) => (
             <Card key={id} className="card-custom">
-                <Card.Img variant="top" src={pathBanner} alt={"preview of the project " + name} className="card-banner" />
-                <Card.Body>
-                    <a onClick={event => handleClick(event, id)}><Card.Title>{name}</Card.Title></a>
-                    <Card.Text>{desc}{frontend}{backend}{type}</Card.Text>
-                    <a href={urlGithub} target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="logo" width="24" height="24" className="mx-2" /></a>
-                    {urlHost != '' ? (<a href={urlHost} target="_blank" rel="noopener noreferrer"><img src={shareIcon} alt="logo" width="24" height="24" className="mx-2" /></a>) : (<></>)}
+                <a onClick={event => handleClick(event, id)}>
+                    <Card.Img variant="top" src={pathBanner} alt={"preview of the project " + name} className="card-banner" />
+                </a>
+                <Card.Body className="d-flex flex-column justify-content-evenly">
+                    <a onClick={event => handleClick(event, id)}>
+                        <Card.Title>{name}</Card.Title>
+                        <Card.Text>{desc}</Card.Text>
+                    </a>
+                    <Card.Text className="mb-0">
+                        <a href={urlGithub} target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="logo" width="24" height="24" className="mx-2" /></a>
+                        {urlHost != '' ? (<a href={urlHost} target="_blank" rel="noopener noreferrer"><img src={shareIcon} alt="logo" width="24" height="24" className="mx-2" /></a>) : null}
+                    </Card.Text>
+                    {type === 'perso' ? (<Card.Text>Projet personnel</Card.Text>) : type === 'OC' ? (<Card.Text>Projet OpenClassrooms</Card.Text>) : null}
                 </Card.Body>
                 <Card.Footer className="d-flex justify-content-around">
                     {frontend ? <p className="mb-0">Frontend</p> : null}
