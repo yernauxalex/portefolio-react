8090dd071c93a90e3945ee4becabe04c575cbf98
Alexis <yernauxalex@gmail.com>
2022-10-11 16:28:36 +0200

    WIP layout modal

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,27 +1,25 @@
 import React from 'react'
-import { Card, Modal } from 'react-bootstrap'
+import { Modal, Stack } from 'react-bootstrap'
 function ProjectCard(props) {
     const data = props.data
 
     return (
-        <Modal show={props.isOpen} onHide={props.toggle} aria-labelledby="contact us form"
+        <Modal show={props.isOpen} onHide={props.toggle} size="lg" dialogClassName="modal-custom" aria-labelledby="contact us form"
             centered>
             <Modal.Header closeButton>
-                <Modal.Title>Contact Us</Modal.Title>
+                <Modal.Title>{data.name}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <Card key={data.id} className="card-custom">
-                    <Card.Img variant="top" src={data.pathBanner} alt={"preview of the project " + data.name} className="card-banner" />
-                    <Card.Body>
-                        <a onClick={data.handleShow}><Card.Title>{data.name}</Card.Title></a>
-                        <Card.Text>{data.desc}{data.frontend}{data.backend}{data.type}</Card.Text>
-                        {/* <a href={urlGithub} target="_blank" rel="noopener noreferrer"><img src={githubIcon} alt="logo" width="24" height="24" className="mx-2" /></a>
-                {urlHost != '' ? (<a href={urlHost} target="_blank" rel="noopener noreferrer"><img src={shareIcon} alt="logo" width="24" height="24" className="mx-2" /></a>) : (<></>)} */}
-                    </Card.Body>
-                    <Card.Footer className="d-flex justify-content-around">
-                        {data.frontend ? <p className="mb-0">Frontend</p> : null}
-                        {data.backend ? <p className="mb-0">Backend</p> : null}
-                    </Card.Footer></Card>
+                <p>Description détaillée {data.desc}</p>
+                {data.tech != null ? (
+                    <>
+                        <h3>Technos utilisées</h3>
+                        <Stack gap={1}>
+                            {data.tech.map(({ name, icon }) =>
+                                <div key={name}><img src={icon} alt={"logo " + name} /><p>{name}</p></div>)}
+                        </Stack>
+                    </>
+                ) : null}
             </Modal.Body>
         </Modal>
     )
