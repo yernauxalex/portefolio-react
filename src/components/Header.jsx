71f6a81584387c8158264fb5c34ec186fba11795
Alexis <yernauxalex@gmail.com>
2022-10-18 17:43:47 +0200

    Fix multiple link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,6 @@
 import React, { useContext } from "react"
 import { LanguageContext } from "../Utils/LanguageContext";
 import { Container, Nav, Navbar } from 'react-bootstrap';
-import linkedinIcon from '../assets/icons/linkedinIcon.png'
-import emailIcon from '../assets/icons/emailIcon.png'
 import franceFlag from '../assets/icons/franceFlag.png'
 import ukFlag from '../assets/icons/ukFlag.png'
 
@@ -21,12 +19,6 @@ function Header() {
                     <Nav className="me-auto">
                     </Nav>
                     <Nav className="d-flex align-items-center">
-
-                        <Nav.Link eventKey={2} href="#contact">
-                            Contact me
-                        </Nav.Link>
-                        <Nav.Link href="https://www.linkedin.com/in/yernauxalexis/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} alt="logo" width="24" height="24" className="mx-2" /></Nav.Link>
-                        <Nav.Link href="mailto: yernauxalex@gmail.com"><img src={emailIcon} alt="logo" width="24" height="24" className="mx-2" /></Nav.Link>
                         <Nav.Item className="d-flex ">
                             <Nav.Link onClick={event => toggleLanguage(event, 'fr-FR')}><img src={franceFlag} alt="logo" width="24" height="24" className="mx-2 mx-lg-0" /></Nav.Link>
                             <Nav.Link onClick={event => toggleLanguage(event, 'en-GB')}><img src={ukFlag} alt="logo" width="24" height="24" className="mx-2 mx-lg-0" /></Nav.Link>
@@ -34,7 +26,7 @@ function Header() {
                     </Nav>
                 </Navbar.Collapse>
             </Container>
-        </Navbar>
+        </Navbar >
     )
 }
 export default Header
